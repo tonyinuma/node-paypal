af39d462476d9ef49eeda3dd8e2ada34f0f49f1e
Tony ID <tony.inuma@gmail.com>
2023-06-04 17:48:34 -0500

    Add: common vars to get host and port

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import mainRoutes from "./routes/main.routes.js";
+import { PORT, HOST } from "./common.js";
 
 const app = express()
-const port = 3000
 
 app.use(mainRoutes)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on ${HOST}`)
 })
