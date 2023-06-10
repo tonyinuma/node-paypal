e122b5251557d0b4ac32e0708118f57189bef042
Tony ID <tony.inuma@gmail.com>
2023-06-10 01:17:08 -0500

    Add: Install dotenv to configure paypal and enviroment variables

--- src/common.js
+++ src/common.js
@@ -1,2 +1,9 @@
+import { config } from "dotenv";
+config();
+
 export const PORT = 3000;
 export const HOST = 'http://localhost:' + PORT;
+
+export const PAYPAL_API_CLIENT_ID = process.env.PAYPAL_API_CLIENT_ID
+export const PAYPAL_API_SECRET_ID = process.env.PAYPAL_API_SECRET_ID
+export const PAYPAL_API = 'https://api-m.sandbox.paypal.com'
