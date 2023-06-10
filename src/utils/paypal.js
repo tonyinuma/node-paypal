754d1aff616fa1cd28433a7ca9e968dfd668f97f
Tony ID <tony.inuma@gmail.com>
2023-06-10 01:50:24 -0500

    Feat: Auth token paypal in utils

---
+++ src/utils/paypal.js
@@ -0,0 +1,20 @@
+import { PAYPAL_API, PAYPAL_API_CLIENT_ID, PAYPAL_API_SECRET_ID } from "../common.js";
+import axios from "axios";
+
+const authTokenPaypal = async () => {
+    const params = new URLSearchParams();
+    params.append('grand_type', 'client_credentials')
+
+    const { data: { access_token } } = axios.post(`${PAYPAL_API}/v1/oauth2/token`, params, {
+        auth: {
+            username: PAYPAL_API_CLIENT_ID,
+            password: PAYPAL_API_SECRET_ID
+        }
+    });
+
+    return access_token;
+}
+
+export {
+    authTokenPaypal
+}
