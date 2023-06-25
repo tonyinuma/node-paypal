bb72a6c35260a53b28715c6b17417d72515c9d7d
Tony ID <tony.inuma@gmail.com>
2023-06-24 19:02:05 -0500

    Feat: generate order link to pay and return to app

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,6 +1,39 @@
+import { HOST, PAYPAL_API } from "../common.js";
+import { authTokenPaypal } from "../utils/paypal.js";
+import axios from "axios";
 
-export const createOrder = (req, res) => {
-    res.send('order created!');
+export const createOrder = async (req, res) => {
+
+    const order = {
+        intent: "CAPTURE",
+        purchase_units: [
+            {
+                amount: {
+                    currency_code: "USD",
+                    value: "120.00"
+                }
+            }
+        ],
+        application_context: {
+            brand_name: "ID Store",
+            landing_page: "NO_PREFERENCE",
+            user_action: "PAY_NOW",
+            return_url: `${HOST}/capture-order`,
+            cancel_url: `${HOST}/cancel-order`
+        }
+    }
+
+    const token = await authTokenPaypal()
+
+    const { data: { links } } = await axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    const { href } = links.find(link => link.rel === 'approve');
+
+    return res.redirect(href);
 }
 
 export const captureOrder = (req, res) => {
